42125c543f40fde77271ccc6dbc8ef02654de9d9
xlboy <984492408@qq.com>
2021-03-04 15:12:41 +0800

    keyof补充

--- ts/ts学习/keyof操作符.ts
+++ ts/ts学习/keyof操作符.ts
@@ -7,5 +7,20 @@ const json = {
 
 type JsonKeys = keyof typeof json
 
+// 原来keyof可以直接将泛型解解解
+// 如果是object类型的，keyof就解出这个object的key
+// 如果是number,boolean,string，就解了个寂寞
+// 但如果是数组的话…也解了个寂寞，解了它自己本身的样子
+type ToRefs<T> = {
+    [K in keyof T]: T[K]
+}
+
+function a<T>(obj: T): ToRefs<T> {
+    return obj
+}
+const aa = ['1', 2]
+
+const cc = a(aa)
+
 // JsonKeys 通过keyof获取出json类型的所有属性名称，即name、age、qq、email
 const str: JsonKeys = 'age'
