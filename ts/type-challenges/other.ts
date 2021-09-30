b65d71e9b9e1b3d81697f7d0de600ec51e77e0f8
xlboy <984492408@qq.com>
2021-09-30 14:38:44 +0800

    新增“获取联合类型的总个数”ts工具类型

--- ts/type-challenges/other.ts
+++ ts/type-challenges/other.ts
@@ -1,15 +1,30 @@
-
-
 /**
  * 将只读的数组，将所有可用的索引变成联合类型
  */
 
-const TypeList = [{}, {}, {}, {}, {}] as const
+const TypeList = [{}, {}, {}, {}, {}] as const;
 
-type GetArrayIndexs<A extends readonly any[], B extends any[] = []> = A extends readonly [
-  infer Start,
-  ...infer Rest
-]
-  ? GetArrayIndexs<Rest, [...B, B['length']]>
-  : B[number]
-type TypeIndex = GetArrayIndexs<typeof TypeList> // 0 | 1 | 2 | 4 | 5
+type GetArrayIndexs<
+  A extends readonly any[],
+  B extends any[] = []
+> = A extends readonly [infer Start, ...infer Rest]
+  ? GetArrayIndexs<Rest, [...B, B["length"]]>
+  : B[number];
+type TypeIndex = GetArrayIndexs<typeof TypeList>; // 0 | 1 | 2 | 4 | 5
+
+/**
+ * 获取联合类型的总个数
+ */
+type Test2 = undefined | boolean | "";
+
+type UtilGetUnionLength<
+  Union,
+  ArrUnion extends any[] = [],
+  S extends Union = Union
+> = [S] extends [never]
+  ? ArrUnion["length"]
+  : S extends S
+  ? UtilGetUnionLength<Exclude<Union, S>, [S, ...ArrUnion]>
+  : never;
+
+type Reu = UtilGetUnionLength<Test2>;
